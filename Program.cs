a2115c30bdd023444d21f8eeefb73905a1c24357
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-25 08:55:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraZnanjaZadatak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi 1. string: ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Unesi 2. string: ");
+            string s2 = Console.ReadLine();
+            if (string.Compare(s1,s2) == 0 || string.Compare(s1, s2) == -1) {
+                Console.WriteLine(s1);
+                Console.WriteLine(s2);
+            }
+            else
+            {
+                Console.WriteLine(s2);
+                Console.WriteLine(s1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
